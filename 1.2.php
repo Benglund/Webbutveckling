e3fb4b71943b24ca54d5f0b2344dd064e96c3acf
Benglund <teodor.englund@gmail.com>
2017-10-19 12:43:37 +0200

    Update 1.2.php

--- 1.2.php
+++ 1.2.php
@@ -1,7 +1,10 @@
 <?php
 header('Content-type: text/plain');
 
-foreach ($_ENV as $key => $value) {
+foreach (getenv() as $key => $value) {
+    echo "$key: $value\n";
+}
+foreach ($_SERVER as $key => $value) {
     echo "$key: $value\n";
 }
 
